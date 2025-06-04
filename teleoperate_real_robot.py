ddbe05c253b6bf56c58e5deb2b8f4ef4bcc9c9d0
HamishMAD <hamishmdawson@gmail.com>
2025-06-04 15:56:41 +0100

    Robot teleoperation now working with real robot

--- teleoperate_real_robot.py
+++ teleoperate_real_robot.py
@@ -1,10 +1,12 @@
 from robot import Robot
 from dynamixel import Dynamixel
 
-leader_dynamixel = Dynamixel.Config(baudrate=1_000_000, device_name='/dev/tty.usbmodem57380045631').instantiate()
-follower_dynamixel = Dynamixel.Config(baudrate=1_000_000, device_name='/dev/tty.usbserial-FT8ISNO8').instantiate()
-follower = Robot(follower_dynamixel, servo_ids=[1, 2, 3, 4, 6, 7])
-leader = Robot(leader_dynamixel, servo_ids=[1, 2, 3, 4, 6, 7])
+#updated device name due to me using windows instead of linux lol
+leader_dynamixel = Dynamixel.Config(baudrate=1_000_000, device_name='COM4').instantiate()
+follower_dynamixel = Dynamixel.Config(baudrate=1_000_000, device_name='COM3').instantiate()
+# had to remove servo id 1 and 7 from follower and leader due to follower id1 motor being broken :(
+follower = Robot(follower_dynamixel, servo_ids=[2, 3, 4, 5, 6])
+leader = Robot(leader_dynamixel, servo_ids=[8, 9, 10, 11, 12])
 leader.set_trigger_torque()
 
 
